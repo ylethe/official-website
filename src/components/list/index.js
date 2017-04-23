b96dec503d793efea1ebc58dcccc8cbb42f2e81a
grace <1253110928@qq.com>
2017-04-23 23:58:00 +0800

    列表页效果完成

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -2,15 +2,39 @@
  * Created by grace on 17-4-22.
  */
 import React ,{Component} from 'react';
+import './index.css';
 
 class List extends Component{
     render(){
         return(
-            <div>
-                List
-            </div>)
+            <div className="ListMain">
+                <div className="content">
+                    <div className="left">学校新闻</div>
+                    <div className="right">
+                        <div className="title">学校新闻</div>
+                        <ul>
+                            <li><a href="#">4•23世界读书日系列活动开幕式暨“赏•指尖上的诗词交响”艺术讲座顺利举行</a><span>2017年04月21日</span></li>
+                            <li><a href="#">4•23世界读书日系列活动开幕式暨“赏•指尖上的诗词交响”艺术讲座顺利举行</a><span>2017年04月21日</span></li>
+                            <li><a href="#">4•23世界读书日系列活动开幕式暨“赏•指尖上的诗词交响”艺术讲座顺利举行</a><span>2017年04月21日</span></li>
+                            <li><a href="#">4•23世界读书日系列活动开幕式暨“赏•指尖上的诗词交响”艺术讲座顺利举行</a><span>2017年04月21日</span></li>
+                            <li><a href="#">4•23世界读书日系列活动开幕式暨“赏•指尖上的诗词交响”艺术讲座顺利举行</a><span>2017年04月21日</span></li>
+                            <li><a href="#">4•23世界读书日系列活动开幕式暨“赏•指尖上的诗词交响”艺术讲座顺利举行</a><span>2017年04月21日</span></li>
+                            <li><a href="#">4•23世界读书日系列活动开幕式暨“赏•指尖上的诗词交响”艺术讲座顺利举行</a><span>2017年04月21日</span></li>
+                            <li><a href="#">4•23世界读书日系列活动开幕式暨“赏•指尖上的诗词交响”艺术讲座顺利举行</a><span>2017年04月21日</span></li>
+                            <li><a href="#">4•23世界读书日系列活动开幕式暨“赏•指尖上的诗词交响”艺术讲座顺利举行</a><span>2017年04月21日</span></li>
+                            <li><a href="#">4•23世界读书日系列活动开幕式暨“赏•指尖上的诗词交响”艺术讲座顺利举行</a><span>2017年04月21日</span></li>
+                            <li><a href="#">4•23世界读书日系列活动开幕式暨“赏•指尖上的诗词交响”艺术讲座顺利举行</a><span>2017年04月21日</span></li>
+                            <li><a href="#">4•23世界读书日系列活动开幕式暨“赏•指尖上的诗词交响”艺术讲座顺利举行</a><span>2017年04月21日</span></li>
+                        </ul>
+                        <div className="foot">
+                            <span>共300页</span>
+                            <button>上一页</button><button>下一页</button>
+                        </div>
 
-
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 export default List;
